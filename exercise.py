be6d337c6c4b27fbd023aaeec7efe6bd59fdb259
darwin.wang <darwin.wang@fil.com>
2022-05-13 17:19:18 +0800

    update

--- exercise.py
+++ exercise.py
@@ -18,3 +18,24 @@ while count < 3:
         count += 1
 else:
     print('Locked')
+
+
+# 求100以内奇数的和
+sum = 0
+for i in range(1, 10, 2):
+    sum += i
+print(f'100以内奇数的和是{sum}')
+
+# 求100以内斐波那契数列的和
+# 0 1 1 2 3
+a = 0
+b = 1
+sum = 0
+for i in range(5):
+    sum = a + b
+    a = b
+    b = sum
+
+print(f'100以内斐波那契数列的和是{sum}')
+
+# 求斐波那契数列第101项的值
