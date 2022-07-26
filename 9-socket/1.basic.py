e5ebe667ae6c2a2bbf3e438cc0a12e0665397086
darwin.wang <darwin.wang@fil.com>
2022-07-26 15:55:41 +0800

    update code

---
+++ 9-socket/1.basic.py
@@ -0,0 +1,58 @@
+'''
+
+Socket套接字，通用的网络编程接口，实现对底层网络接口的封装
+在网络协议出现之后为了更方便的实现网络的IO而实现socket编程
+基于网络协议之上的，高级抽象，方便的网络编程接口，而不是从底层开发
+
+应用的进程之间需要借助网络端口搭通道来解决端到端通信
+
+
+参数：
+AF是Address Family，有AF_INET的IPv4 和 AF_INET6的IPv6
+SOCK_STREAM，面向连接的TCP
+SOCK_DGRAM，无连接的UDP
+
+服务端编程流程：
+创建socket对象
+bind地址和端口
+listen进行监听，实现三次握手连接
+accept，建立连接后接受客户端并底层创建新的socket对象和客户端通信
+返回元组类型，有对端和本地的ip地址和端口号
+recv和send，用新的socket对象和客户端通信
+
+很多方法都是阻塞，因此用多线程比较好
+
+
+'''
+
+import socket
+
+# 创建socket对象
+server = socket.socket()
+print(server)
+
+# 监听的地址和端口
+addr = ('127.0.0.1', 9999)
+
+# 绑定到服务端
+server.bind(addr)
+
+# 开启服务端监听，不会阻塞
+server.listen()
+
+# 阻塞方法，等待连接，如果有连接来则返回新的socket对象
+newsock, raddr = server.accept()
+print(newsock, raddr)
+print(newsock.getpeername())
+print(newsock.getsockname())
+
+
+# # 接受数据，是阻塞方法
+data = newsock.recv(1024)
+print(type(data), data)
+
+# # 发送数据
+newsock.send(f'!!!!{data}!!!'.encode())
+
+# # 关闭socket连接
+server.close()
---
+++ 9-socket/2.chat.py
@@ -0,0 +1,43 @@
+import socket
+import threading
+
+
+class ChatServer:
+
+    def __init__(self, ip='127.0.0.1', port=9999):
+        self.addr = ip, port
+        self.sock = socket.socket()
+        self.event = threading.Event()
+
+    def start(self):
+        self.sock.bind(self.addr)
+        self.sock.listen()
+        # 防止主线程阻塞
+        threading.Thread(target=self.accept, name='accept').start()
+
+    # accept会在一个线程中循环监听连接，如果有连接进入则会开辟新线程
+    def accept(self):
+        while not self.event.is_set():
+            new_sock, raddr = self.sock.accept()
+            threading.Thread(target=self.recv, name='recv', args=(new_sock, raddr)).start()
+
+    def recv(self, sock, raddr):
+        while not self.event.is_set():
+            data = sock.recv(1024)
+            if data == b'' or data == b'q':
+                break
+            sock.send('from {}, data is {}'.format(*raddr, data).encode())
+
+    def stop(self):
+        self.event.set()
+        self.sock.close()
+
+
+if __name__ == '__main__':
+    cs = ChatServer()
+    cs.start()
+    while True:
+        cmd = input('>>>')
+        if cmd == 'q':
+            cs.stop()
+            break
